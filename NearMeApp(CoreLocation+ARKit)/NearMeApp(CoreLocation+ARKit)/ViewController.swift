4b2c26d8e2e36b76cb21902c8fac4328355cde7f
imnikita1 <97101755+imnikita1@users.noreply.github.com>
2023-09-04 09:01:12 +0200

    initialCommit

---
+++ NearMeApp(CoreLocation+ARKit)/NearMeApp(CoreLocation+ARKit)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMeApp(CoreLocation+ARKit)
+//
+//  Created by Mykyta Popov on 04/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
