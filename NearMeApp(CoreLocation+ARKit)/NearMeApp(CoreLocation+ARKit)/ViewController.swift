df588d22ce98c9d805f305284bdbbdf8b7052020
imnikita1 <97101755+imnikita1@users.noreply.github.com>
2023-09-04 09:21:12 +0200

    TableView and navigation setup

--- NearMeApp(CoreLocation+ARKit)/NearMeApp(CoreLocation+ARKit)/ViewController.swift
+++ NearMeApp(CoreLocation+ARKit)/NearMeApp(CoreLocation+ARKit)/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var place: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = place
     }
 
 
